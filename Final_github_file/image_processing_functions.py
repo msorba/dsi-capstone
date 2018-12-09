191d3a73fe0376423f5e31e36348ad1826f9bba5
Marianne Sorba <msorba@MV-ML-f45c89a2e1a7.local>
2018-12-09 12:05:47 -0500

    finish image processing tutorial

--- Final_github_file/image_processing_functions.py
+++ Final_github_file/image_processing_functions.py
@@ -1,73 +1,13 @@
 import os, re
 import numpy as np
 import pandas as pd
+from math import sqrt
 import cv2
 from PIL import Image
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import pylab
 
-
-basepath = "Images"
-
-def get_data_from_filenames(basepath):
-    """
-    uses file structure and filename to extract relevant information.
-    basepath:: the relative directory to scan for images. basepath must contain a (sub-)directory called 'Images'
-    returns: dataframe containing information from filenames
-    """
-    full_data = []
-    for root, dirs, files in os.walk(basepath, topdown=True):
-        for name in files:
-            data = name.split(".")[0]
-            data = data.split("_")
-            method = root.split("/")[root.split("/").index("Images") + 1]
-            data.insert(0,method)            #add method from directory name to data
-            if method == 'Scanner':
-                if len(data) < 5:            # make all data consistent length
-                    data.append("-")
-                if data[4].isdigit():
-                    data.insert(4,"-")       # some filenames include background color (W or B). This accounts for missing values.
-                if len(data) == 5:
-                    data.append('0')         # some filepaths have "_1" or "_2" at the end. This adds "0" for missing values. 
-            data[3] = int(data[3][1])
-
-            if method == 'Keyence':          # images in Keyence directory had simpler format. Adding NAs for data consistency,
-                data.append("NA")
-                data.append("NA")
-
-            data.append(os.path.join(root,name))
-            full_data.append(data)
-    return pd.DataFrame(full_data,columns=['method','genotype','date','day','background_color','img_num??','filepath'])
-
-
-def load_images(file_list_or_directory,size=(1000,1000)):
-    """
-    file_list_or_directory:: (string, list, pd.core.series)::
-                            you can give a list of full filepaths (using 'filepath' column from get data function dataframe)
-                            OR a string to specify diectory
-    returns: array of image arrays
-    """
-    images = []
-    
-    if type(file_list_or_directory) == str:
-        for root, dirs, files in os.walk(file_list_or_directory, topdown=True):
-            for name in files:
-                mypath = os.path.join(root,name)
-                img = Image.open(mypath)
-                img = img.resize(size)
-                arr = np.array(img).astype('uint8')
-                images.append(arr)
-    else:
-        for file in file_list_or_directory:
-            img = Image.open(file)
-            img = img.resize(size)
-            arr = np.array(img).astype('uint8')
-            images.append(arr)
-    return images
-
-
-# This function transforms cartesian coordinates to polar. X is the image (has to be array)
 
 def polar_coord(X):
     """
@@ -81,7 +21,6 @@ def polar_coord(X):
     return(X_polar)
 
 
-# This function transforms polar coordinates to cartesian. X is the image (has to be array)
 def cartesian_coord(X):
     """
     X: image (array format) 
@@ -94,7 +33,7 @@ def cartesian_coord(X):
     return(X_cartesian)
 
 
-def get_wrinkles(img, resize = 128, movie = False, background_is_black = True):
+def get_wrinkles(img, resize = 500, movie = False, background_is_black = True):
     
     """
     img: initial image
@@ -146,7 +85,7 @@ def get_wrinkles(img, resize = 128, movie = False, background_is_black = True):
     return(labels, classes)
 
 
-def perc_wrinkled(img, background_is_black = True, resize = 128):
+def perc_wrinkled(img, background_is_black = True, resize = 500):
     
     """
     input : initial img
@@ -158,7 +97,7 @@ def perc_wrinkled(img, background_is_black = True, resize = 128):
 
 
 
-def detect_spokes(img, rho = 1, theta = np.pi/180, minLineLength = 3, maxLineGap = 1, resize = 128, threshold = 10):
+def detect_spokes(img, rho = 1, theta = np.pi/180, minLineLength = 10, maxLineGap = 2 , resize = 500, threshold = 10):
     """
     input img
     
@@ -168,62 +107,25 @@ def detect_spokes(img, rho = 1, theta = np.pi/180, minLineLength = 3, maxLineGap
     polar_img_wrinkle = polar_coord(img_wrinkle)
     edges = cv2.Canny(polar_img_wrinkle,0,1) 
     edges = cv2.dilate(edges,cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2)))
-    lines = cv2.HoughLinesP(edges,rho=rho,theta=theta, threshold = threshold, minLineLength = 
-                            int((minLineLength * resize) / 50),maxLineGap=int((maxLineGap * resize) / 50))
+    lines = cv2.HoughLinesP(edges,rho=rho,theta=theta, threshold = threshold, minLineLength = int(minLineLength * resize) / 100,
+                            maxLineGap = int(maxLineGap * resize) / 100)
     count = 0
     if lines is None:
-        return(img,0)
+        return(img, 0, 0)
     else:
         img_return = polar_coord(img)
+        medium_length = []
+        medium_dist_center = []
         for line in lines:
             coords = line[0]
             slope = (coords[3]-coords[1])/(coords[2]-coords[0])
-            if slope > -0.3 and slope <0.3:
-                cv2.line(img_return,(coords[0],coords[1]),(coords[2],coords[3]),[250,250,250],int(resize / 300))
+            length = sqrt((coords[3] - coords[1])**2 + (coords[2] - coords[0])**2)
+            if slope > -0.2 and slope <0.2 and coords[0] > resize / 4 :
+                cv2.line(img_return,(coords[0],coords[1]),(coords[2],coords[3]),[250,250,250],int( resize / 200))
                 count += 1
+                medium_length.append(length)
+                medium_dist_center.append(min(coords[0], coords[3]))
     img_return = cartesian_coord(img_return)
-    return(img_return, count)
-
-
-def plot_wrinkle_class(img_wrinkle_class, save=True):
-    """
-    input : wrinkle class img
-    
-    returns: plots the wrinkle class img and saves it if save = True
-    """
-    plt.figure(num=None, figsize=(10, 10), dpi=80, facecolor='w', edgecolor='k')
-    plt.imshow(img_wrinkle_class)
-
-    if save:
-        pylab.savefig('static/results/wrinkle.png',bbox_inches='tight')
-
-
-
-def detect_lines_cy(img, rho=1, theta=np.pi / 180, threshold=150, minLineLength=5, maxLineGap=1):
-    """
-    input img
-
-    returns: image with lines and lines
-    """
-    polar_img_wrinkle = polar_coord(get_wrinkle_class(img)[1])
-    n_pixels = img.shape[0] + img.shape[1]
-    edges = cv2.Canny(polar_img_wrinkle, 40, 70)
-    edges = cv2.dilate(edges, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2)))
-    lines = cv2.HoughLinesP(edges, rho=rho, theta=theta, threshold=threshold,
-                            minLineLength=int(minLineLength * n_pixels / 100),
-                            maxLineGap=int(maxLineGap * n_pixels / 100))
-    pol_lines = []
-    if lines is None:
-        return None, []
-    else:
-        img_return = polar_coord(img)
-        for line in lines:
-            coords = line[0]
-            slope = (coords[3] - coords[1]) / (coords[2] - coords[0])
-            if slope > -0.2 and slope < 0.2:
-                cv2.line(img_return, (coords[0], coords[1]), (coords[2], coords[3]), [250, 250, 250],
-                         int(n_pixels / 500))
-                pol_lines.append(line)
-    img_return = cartesian_coord(img_return)
-    return (img_return, pol_lines)
-
+    medium_length = np.median(np.array(medium_length))
+    medium_dist_center = np.median(np.array(medium_dist_center))
+    return(img_return, count, medium_length, medium_dist_center)
