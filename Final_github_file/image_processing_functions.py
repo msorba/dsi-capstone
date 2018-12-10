456aade56e9e98c1da67c635a90905d47e74a95e
Marianne Sorba <msorba@MV-ML-f45c89a2e1a7.local>
2018-12-10 18:27:26 -0500

    video tutorial

--- Final_github_file/image_processing_functions.py
+++ Final_github_file/image_processing_functions.py
@@ -32,7 +32,8 @@ def cartesian_coord(X):
     return(X_cartesian)
 
 
-def get_wrinkles(img, resize = 500, movie = False, background_is_black = True):
+def get_wrinkles(img, resize = 500, is_movie = False, is_red = True, background_is_black = True, threshold_val=100, alpha=80, 
+                 beta=100):
     
     """
     img: initial image
@@ -46,63 +47,77 @@ def get_wrinkles(img, resize = 500, movie = False, background_is_black = True):
         pass
     
     # Resize
-    if not movie:
-        size = (resize, resize)
-        img_resize       = img.resize(size, Image.ANTIALIAS)#resize
-        img_array_resize = np.asarray(img_resize)
-    else:
-        reduce_factor    = 4
-        size             = tuple(np.divide(img.shape[:2], reduce_factor).astype(int)) + (3,)
-        img_array_resize = np.resize(img, size)
 
-
-    # Reshape
-    w,h,d = tuple(img_array_resize.shape)
-    X     = np.reshape(img_array_resize, (w*h,d))
-
-    # Add radial distance as feature
-    y_axis = np.array([])
-    for i in np.arange(resize):
-        y_axis = np.append(y_axis, np.arange(resize))
-        
-    y_axis          = np.reshape(y_axis, (resize**2, 1))
-    x_axis          = np.reshape(np.repeat(np.arange(resize), resize), (resize**2, 1))
-    radial_distance = np.sqrt(np.abs(x_axis - (resize / 2))**2 + np.abs(y_axis - (resize / 2))**2)
-    X               = np.append(X, radial_distance, axis=1)
+    size = (resize, resize)
+    img_resize       = img.resize(size, Image.ANTIALIAS)#resize
+    img_array_resize = np.asarray(img_resize)
     
-    # Determine labels and classes
-    img_array = np.asarray(img)
-    w,h,d     = tuple(img_array.shape)
-    labels = ((X[:,0] > 100) * 1) & np.abs((1 - (X[:,1] / X[:,2])) < 0.75) & (X[:,1] < 80) & (X[:,2] < 80) & ((X[:,0]/ (X[:,1] +  X[:,2])) > 1.0)  
+    
+    
+    if is_movie or not is_red :
+        
+        a = img_array_resize.mean(axis=2) < alpha
+        b = img_array_resize.max(axis=2) > beta
+        fil = a*b
+        classes = fil * img_array_resize.max(axis=2)
+        classes[classes > 0] = 1
+        labels = np.reshape(classes, resize * resize)
+        
+    else:
+
+        # Reshape
+        w,h,d = tuple(img_array_resize.shape)
+        X     = np.reshape(img_array_resize, (w*h,d))
+
+        # Add radial distance as feature
+        y_axis = np.array([])
+        for i in np.arange(resize):
+            y_axis = np.append(y_axis, np.arange(resize))
+
+        y_axis          = np.reshape(y_axis, (resize**2, 1))
+        x_axis          = np.reshape(np.repeat(np.arange(resize), resize), (resize**2, 1))
+        radial_distance = np.sqrt(np.abs(x_axis - (resize / 2))**2 + np.abs(y_axis - (resize / 2))**2)
+        X               = np.append(X, radial_distance, axis=1)
+
+        # Determine labels and classes
+        img_array = np.asarray(img)
+        w,h,d     = tuple(img_array.shape)
+        labels = ((X[:,0] > 100) * 1) & np.abs((1 - (X[:,1] / X[:,2])) < 0.75) & (X[:,1] < 80) & (X[:,2] < 80) & ((X[:,0]/ (X[:,1] +  X[:,2])) > 1.0) 
+        classes   = np.reshape(labels, size)
+        
+    # apply black mask to background    
     lower_black = np.array([0,0,0], dtype = "uint16")
     upper_black = np.array([40,40,40], dtype = "uint16")
     black_mask = cv2.inRange(img_array_resize, lower_black, upper_black)
-    classes   = np.reshape(labels, size)
+
     
-    if background_is_black == True :
-        classes[black_mask > 0] = -1
+    if background_is_black :
+        classes[black_mask > 0] = 3
+        
     return(labels, classes)
 
 
-def perc_wrinkled(img, background_is_black = True, resize = 500):
+def perc_wrinkled(img, resize = 500, is_movie = False, is_red = True, background_is_black = True):
     
     """
     input : initial img
     
     returns: the percent of the surface of the biofilm covered by wrinkles
     """
-    wrinkle_labels, _ = get_wrinkles(img, background_is_black = background_is_black, resize = resize)
-    return round(100*sum(np.array(wrinkle_labels) == 1) / sum(np.array(wrinkle_labels) == 0), 2)
+    wrinkle_labels, _ = get_wrinkles(img, resize = resize, is_movie = is_movie, is_red = is_red, 
+                                     background_is_black = background_is_black)
+    return round(100*sum(np.array(wrinkle_labels) == 1) / (sum(np.array(wrinkle_labels) == 0) + sum(np.array(wrinkle_labels) == 
+                                                                                                   1)), 2)
 
 
 
-def detect_spokes(img, rho = 1, theta = np.pi/180, minLineLength = 10, maxLineGap = 2 , resize = 500, threshold = 10):
+def detect_spokes(img, resize = 500, is_movie = False, is_red = True, rho = 1, theta = np.pi/180, minLineLength = 10, maxLineGap = 2 , threshold = 10):
     """
     input img
     
     returns: the initial images with the lines, the number of lines
     """
-    _, img_wrinkle = get_wrinkles(img, resize = resize, background_is_black = False)
+    _, img_wrinkle = get_wrinkles(img, resize = resize, is_movie = is_movie, is_red = is_red, background_is_black = False)
     polar_img_wrinkle = polar_coord(img_wrinkle)
     edges = cv2.Canny(polar_img_wrinkle,0,1) 
     edges = cv2.dilate(edges,cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2)))
